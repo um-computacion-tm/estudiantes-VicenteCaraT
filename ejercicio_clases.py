b616ad28b6b01503c3e4956d6d6b6356878867a6
VicenteCaraT <vit.cara@alumno.um.edu.ar>
2023-04-25 14:34:28 -0300

    Segundo Commit: prints ejercicio_calses.py

--- ejercicio_clases.py
+++ ejercicio_clases.py
@@ -7,7 +7,7 @@ class Persona:
 class Alumno(Persona):#Alumno hereda de la clase persona
     def __init__(self, param_nombre,_param_apellido,param_DNI,param_Legajo,param_Tel,para_email,param_direccion):
         self.nombre = param_nombre
-        self.apellid = _param_apellido
+        self.apellido = _param_apellido
         self.DNI = param_DNI
         self.Legajo = param_Legajo
         self.Tel = param_Tel
@@ -16,8 +16,8 @@ class Alumno(Persona):#Alumno hereda de la clase persona
 
 
 class Materia:
-    def __init__(self, param_nombremat, param_idmat, param_horario,param_duracion,):
-        self.nombremat = param_nombremat
+    def __init__(self, param_nombre, param_idmat, param_horario,param_duracion,):
+        self.nombre = param_nombre
         self.idmat = param_idmat
         self.horario = param_horario
         self.duracion = param_duracion
@@ -25,18 +25,35 @@ class Materia:
 
 
 class Profesor:
-    def __init__ (self,param_profid,param_nombreprof,param_apellido,param_email,param_telefono):
+    def __init__ (self,param_profid,param_nombre,param_apellido,param_email,param_telefono):
         self.idProf = param_profid
-        self.nombreprof = param_nombreprof
+        self.nombre = param_nombre
         self.apellido = param_apellido
         self.email = param_email
         self.telefono = param_telefono
 
 
 if __name__ == '__main__':
-    alumno_vicente = Alumno ("Vicente", "Cara Tapia", 44909938, 65432, "299-656-3936","vit.cara@alumno.um.edu.ar", "Los Copihues 135")
-    alumno_tomas = Alumno ("Tomas", "Rojas", 44909934, 65789, "299-656-3936", "t.rojas@alumno.um.edu.ar", "Colonia Cebolla 1600")
-    materia_CalculoI = Materia ("Calculo I", 4567, "Lunes y Miercoes a las 3pm a 5", "semestral")
-    materia_Algebra = Materia ("Algebra Analítica", 4587,  "Martes a las 3pm a 7", "anual")
-    profe_Tomas = Profesor (4321, "Tomas", "Schugurensky", "elmontedeshugoslavia@edu.ar", "261-435-1231")
-    profe_Franco = Profesor (4323, "Franco", "Garcia", "franquilloproxdomg@gmail.com", "261-123-4123")
+    alumno_vicente = Alumno ("Vicente", "Cara Tapia",  '44909938',  '65432', "299-656-3936","vit.cara@alumno.um.edu.ar", "Los Copihues 135")
+    print('Alumno: ' + alumno_vicente.nombre)
+    print('Apellido: ' + alumno_vicente.apellido)
+    print('D.N.I: ' + alumno_vicente.DNI)
+    print('Legajo: ' + alumno_vicente.Legajo)
+    print('teléfono: ' + alumno_vicente.Tel)
+    print('Email: ' + alumno_vicente.email)
+    print('Direccion: ' + alumno_vicente.direccion)
+
+
+    materia_CalculoI = Materia ("Calculo I", '4567', "Lunes y Miercoes a las 3pm a 5pm", "Semestral")
+    print('Nombre de Materia: ' + materia_CalculoI.nombre)
+    print('IdMateria: ' + materia_CalculoI.idmat)
+    print('Horario: ' + materia_CalculoI.horario)
+    print('Duracion: ' + materia_CalculoI.duracion)
+
+
+    profe_Tomas = Profesor ('4321', "Tomas", "Schugurensky", "elmontedeshugoslavia@edu.ar", "261-435-1231")
+    print('idProfe: ' + profe_Tomas.idProf)
+    print('Nombre: ' + profe_Tomas.nombre)
+    print('Apellido: ' + profe_Tomas.apellido)
+    print('Emaio: ' + profe_Tomas.email)
+    print('Teléfono: ' + profe_Tomas.telefono)
